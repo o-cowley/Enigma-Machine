88a67e7cffb8411b8cf048de7c8a687edb2e203d
olivercowley <ocowley@student.ubc.ca>
2021-11-09 21:09:52 -0800

    added LookAndFeel Options

--- src/main/ui/Tools/SaveLoadPopUp.java
+++ src/main/ui/Tools/SaveLoadPopUp.java
@@ -3,6 +3,7 @@ package ui.Tools;
 import model.InUseRotors;
 
 import javax.swing.*;
+import java.lang.reflect.Array;
 
 // A PopUp that prompts a user to either load saved rotor settings, or to save the settings at the end
 public class SaveLoadPopUp extends JFrame {
@@ -23,16 +24,35 @@ public class SaveLoadPopUp extends JFrame {
     public void initAction(boolean save) {
         if (save) {
             result = buttons.showConfirmDialog(this, "Would you like to load your settings?",
-                    "", JOptionPane.YES_NO_OPTION);
+                    "", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 
         } else {
             result = buttons.showConfirmDialog(this, "Would you like to save your settings?",
-                    "", JOptionPane.YES_NO_OPTION);
+                    "", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
         }
 
     }
 
+    //javax.swing.plaf.metal.MetalLookAndFeel
+    //com.sun.java.swing.plaf.motif.MotifLookAndFeel    great!!
+    //javax.swing.plaf.nimbus.NimbusLookAndFeel        great!!
+    //com.sun.java.swing.plaf.gtk.GTKLookAndFeel   only windows
+    //
+    //com.sun.java.swing.plaf.windows.WindowsLookAndFeel  windows only
+    //com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel
+
+    //com.apple.laf.AquaLookAndFeel  mac only
+    // com.sun.java.swing.plaf.gtk.GTKLookAndFeel   note windows i think
+
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            System.out.println("couldn't load what you wanted");
+        }
         new SaveLoadPopUp(true);
+        for (UIManager.LookAndFeelInfo e: UIManager.getInstalledLookAndFeels()) {
+            System.out.println(e.getName());
+        }
     }
 }
